6569004d337049f3dbdd2bcd74e07951edc7d85b
Gideon Crawley <gscrawley@gmail.com>
2021-07-14 17:39:46 -0700

    map reader and grid done next make image loader and separate pixels

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # iterative_flood_fill_CS_2.2_final
+
+# For this i'll be using what's known as the flood/fill algorithm. 
+
+# it's what's used in MineCraft to find mines as well as the Paint Bucket tool in Paint Programs.
+
+# Starting from a pixel at (x,y) location, it explores an image pixel by pixel, 
+# by visiting the neighbouring pixels of any pixel it previously visited. 
+
+# Given a target color and a replacement color, each pixel matching the target color gets repainted the replacement color. 
+
+# if the target color isn't found, then the algorithm ends. 
+
+# Due to time constraints I won't be including a visual representation, instead I'll demonstrate that its working by having the program display the number of 'islands' of color on a white 'map'.
---
+++ map_reader.py
@@ -0,0 +1,68 @@
+class MapReader:
+    '''
+    Reads the reduced map (1s and 0s), where 1 - island and 0 - sea
+    '''
+    def is_one(self, grid, row, col):
+        '''
+        Helper method for checking whether the pixel belongs to an island or not
+        '''
+        if (row < 0 or row > len(grid) - 1):
+            return False
+
+        if (col < 0 or col > len(grid[0]) - 1):
+            return False
+
+        if grid[row][col] == 1:
+            return True
+        else:
+            return False
+
+    def iterative_flood_fill(self, grid, row, col):
+        '''
+        Iterative version of flood fill algorithm. Works better for larger maps.
+        '''
+        if (row < 0 or row > len(grid) - 1):
+            return
+
+        if (col < 0 or col > len(grid[0]) - 1):
+            return
+
+        if (grid[row][col] != 1):
+            return
+
+        q = []  # init empty queue
+        grid[row][col] = -1  # mark as visited
+        q.append([row, col])  # add to queue
+
+        while len(q) > 0:
+            [cur_row, cur_col] = q[0]
+            del q[0]
+
+            if (self.is_one(grid, cur_row - 1, cur_col) == True):
+                grid[cur_row - 1][cur_col] = -1
+                q.append([cur_row - 1, cur_col])
+
+            if (self.is_one(grid, cur_row + 1, cur_col) == True):
+                grid[cur_row + 1][cur_col] = -1
+                q.append([cur_row + 1, cur_col])
+
+            if (self.is_one(grid, cur_row, cur_col - 1) == True):
+                grid[cur_row][cur_col - 1] = -1
+                q.append([cur_row, cur_col - 1])
+
+            if (self.is_one(grid, cur_row, cur_col + 1) == True):
+                grid[cur_row][cur_col + 1] = -1
+                q.append([cur_row, cur_col + 1])
+
+    def count_islands(self, reduced_map):
+        '''
+        Main method for counting islands on the map
+        '''
+        islands = 0
+
+        for row in range(len(reduced_map)):
+            for col in range(len(reduced_map[0])):
+                if (reduced_map[row][col] == 1):
+                    islands += 1
+                    self.iterative_flood_fill(reduced_map, row, col)
+        return islands
